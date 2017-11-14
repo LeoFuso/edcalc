c82f36b819f3cbac633ece6bd051dbe8e7dc9f5a
LeoFuso <leonardofusonuzzo@gmail.com>
2017-11-14 21:17:09 -0200

    Primeiro commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Starting calculations..." << std::endl;
+
+    return 0;
+}
+
+static inline double distanceNaive(double *v1, double *v2, int n){
+    return 0.0;
+}
+
+static inline double distanceHypot(double *v1, double *v2, int n){
+    return 0.0;
+}
+
+static inline double distanceFast(double *v1, double *v2, int n){
+    return 0.0;
+}
