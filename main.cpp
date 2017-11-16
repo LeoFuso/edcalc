3008f7ca83600760d5335bf8de6d5c1871c0e492
LeoFuso <leonardofusonuzzo@gmail.com>
2017-11-16 15:46:21 -0200

    top

--- main.cpp
+++ main.cpp
@@ -1,26 +1,48 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
+
+struct Test {
+    double test1;
+    double test2;
+    double test3;
+
+    void Reset() {
+        test1 = test2 = test3 = 0;
+    }
+};
+
+void fill_vector(vector<double> &values);
 
 int main() {
 
     size_t size = 1000000;
 
-    vector<double> *vector = new vector<double> (size);
+    vector<double> values;
+
+    fill_vector(values);
 
     cout << "Starting calculations..." << endl;
 
     return 0;
 }
 
-static inline double distanceNaive(double *v1, double *v2, int n){
+static inline double distanceNaive(double *v1, double *v2, int n) {
     return 0.0;
 }
 
-static inline double distanceHypot(double *v1, double *v2, int n){
+static inline double distanceHypot(double *v1, double *v2, int n) {
     return 0.0;
 }
 
-static inline double distanceFast(double *v1, double *v2, int n){
+static inline double distanceFast(double *v1, double *v2, int n) {
     return 0.0;
+}
+
+void fill_vector(vector<double> &values) {
+    for (int i = 0; i < 1000000; ++i)
+        values.push_back(i * 1.0);
+
+
 }
