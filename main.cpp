1c4071ac9301d856b03a0c658b6ae410e5f74916
LeoFuso <leonardofusonuzzo@gmail.com>
2017-11-16 17:14:53 -0200

    Função euclidiana naive implementada

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <math.h>
 
 using namespace std;
 
@@ -14,6 +15,7 @@ struct Test {
 };
 
 void fill_vector(vector<double> &values);
+static inline double distanceNaive(vector<double> &values, int n);
 
 int main() {
 
@@ -21,15 +23,27 @@ int main() {
 
     vector<double> values;
 
+    cout << "Filling vector for calculations..." << endl;
     fill_vector(values);
-
+    cout << "OK!" << endl;
     cout << "Starting calculations..." << endl;
+    cout << distanceNaive(values, 699000) << endl;
 
     return 0;
 }
 
-static inline double distanceNaive(double *v1, double *v2, int n) {
-    return 0.0;
+static inline double distanceNaive(vector<double> &values, int n) {
+    double result = 0;
+
+    int distance = n/2;
+    double pn = 0;
+    double qn = 0;
+    for (int i = 0; i < n; ++i) {
+        pn = values[i];
+        qn = values[i+distance];
+        result += (pn - qn)*(pn - qn);
+    }
+    return sqrt(result);
 }
 
 static inline double distanceHypot(double *v1, double *v2, int n) {
@@ -42,7 +56,5 @@ static inline double distanceFast(double *v1, double *v2, int n) {
 
 void fill_vector(vector<double> &values) {
     for (int i = 0; i < 1000000; ++i)
-        values.push_back(i * 1.0);
-
-
+        values.push_back(i + 1.0);
 }
