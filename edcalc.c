ced199ecfd20a469768c0db08ec1673ecef365f1
LeoFuso <leonardofusonuzzo@gmail.com>
2017-11-30 11:49:14 -0200

    headers e source file criados para euclydian distance

---
+++ edcalc.c
@@ -0,0 +1,10 @@
+/*
+ * File:   edcalc.c
+ * Author: lfuso
+ * From: https://github.com/LeoFuso/edcalc
+ *
+ * Created on 2017-11-30
+ */
+
+#include "edcalc.h"
+
---
+++ edcalc.h
@@ -0,0 +1,9 @@
+/*
+ * File:   edcalc.c
+ * Author: lfuso
+ * From: https://github.com/LeoFuso/edcalc
+ *
+ * Created on 2017-11-30
+ */
+
+#pragma once
