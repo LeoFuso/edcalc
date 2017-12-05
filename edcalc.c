f5e8d906868ad7d9bae7d9b8e34bfe292fa7826c
LeoFuso <leonardofusonuzzo@gmail.com>
2017-12-05 08:46:45 -0200

    inlining failed in class to always_inline

--- edcalc.c
+++ edcalc.c
@@ -33,27 +33,27 @@ double euclidean_baseline(const double *x,const double *y, size_t n)
 double e_intrinsic(const double *x,const double *y, size_t n)
 {
     double result = 0;
-    __m128d euclidean = _mm_setzero_ps();
+    __m256d euclidean = _mm256_setzero_pd();
 
     for (; n>3; n-=4) {
-        const __m128d a = _mm_loadu_pd(x);
-        const __m128d b = _mm_loadu_pd(y);
-        const __m128d sub = _mm_sub_pd(a,b);
-        const __m128d sqrt = _mm_mul_pd(sub, sub);
-        euclidean = _mm_add_pd(euclidean, sqrt);
+        const __m256d a = _mm256_load_pd(x);
+        const __m256d b = _mm256_load_pd(y);
+        const __m256d sub = _mm256_sub_pd(a,b);
+        const __m256d sqrt = _mm256_mul_pd(sub, sub);
+        euclidean = _mm256_add_pd(euclidean, sqrt);
         x+=4;
         y+=4;
     }
-    const __m128d shuffle1 = _mm_shuffle_pd(euclidean, euclidean, _MM_SHUFFLE(1,0,3,2));
-    const __m128d sum1 = _mm_add_pd(euclidean, shuffle1);
-    const __m128 shuffle2 = _mm_shuffle_ps(sum1, sum1, _MM_SHUFFLE(2,3,0,1));
-    const __m128 sum2 = _mm_add_ps(sum1, shuffle2);
-    _mm_store_ss(&result,sum2);
+    const __m256d shuffle1 = _mm256_shuffle_pd(euclidean, euclidean, _MM_SHUFFLE(1,0,3,2));
+    const __m256d sum1 = _mm256_add_pd(euclidean, shuffle1);
+    const __m256d shuffle2 = _mm256_shuffle_pd(sum1, sum1, _MM_SHUFFLE(2,3,0,1));
+    const __m256d sum2 = _mm256_add_pd(sum1, shuffle2);
+    _mm256_store_pd(&result,sum2);
     //    _mm_empty();
     if (n)
         result += euclidean_baseline(x, y, n);	// remaining 1-3 entries
 
-    return result;
+    return sqrt(result);
 }
 
 double euclidean_sse(const double *x, const double *y, size_t dim)
