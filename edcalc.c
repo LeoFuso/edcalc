79ad828b967c67204ff79ed05cbcc0fce3b14b4c
LeoFuso <leonardofusonuzzo@gmail.com>
2017-12-10 00:29:54 -0200

    Os resultados são consistentes agora.

--- edcalc.c
+++ edcalc.c
@@ -44,12 +44,11 @@ double euclidean_256d(const double *x,const double *y, size_t n)
         x+=4;
         y+=4;
     }
-    const __m256d shuffle1 = _mm256_shuffle_pd(euclidean, euclidean, _MM_SHUFFLE(0,0,1,1)); // Latency: 1 Throughput: 1
-    const __m256d sum1 = _mm256_add_pd(shuffle1, euclidean);                                // Latency: 4 Throughput: 0.5
-    const __m128d higherEnd= _mm256_extractf128_pd(sum1,1);                                 // Latency: 3 Throughput: 1
-    const __m256d castedHigherEnd =  _mm256_castpd128_pd256(higherEnd);                     // No cost
-    const __m256d sum2 = _mm256_add_pd(sum1, castedHigherEnd);                              // Latency: 4 Throughput: 0.5
-    result =  _mm256_cvtsd_f64(sum2);                                                       //         12             3
+    const __m256d shuffle = _mm256_shuffle_pd(euclidean, euclidean, _MM_SHUFFLE(0,0,1,1)); // Latency: 1 Throughput: 1
+    const __m256d sum1 = _mm256_add_pd(shuffle, euclidean);                                // Latency: 4 Throughput: 0.5
+    const __m256d permute= _mm256_permute2f128_pd(sum1,sum1,1);                            // Latency: 3 Throughput: 1
+    const __m256d sum2 = _mm256_add_pd(sum1, permute);                                     // Latency: 4 Throughput: 0.5
+    result =  _mm256_cvtsd_f64(sum2);                                                      //         12             3
     if (n)
         result += euclidean_remaining(x, y, n);	// remaining 1-3 entries
 
@@ -58,7 +57,6 @@ double euclidean_256d(const double *x,const double *y, size_t n)
 
 double euclidean_128d(const double *x, const double *y, size_t n)
 {
-
     double result = 0;
     __m128d euclidean = _mm_setzero_pd();
 
@@ -71,15 +69,11 @@ double euclidean_128d(const double *x, const double *y, size_t n)
         x+=2;
         y+=2;
     }
-    const __m128d shuffle1 = _mm_shuffle_pd(euclidean, euclidean, _MM_SHUFFLE(1,0,3,2));
-    const __m128d sum1 = _mm_add_pd(euclidean, shuffle1);
-    const __m128d shuffle2 = _mm_shuffle_pd(sum1, sum1, _MM_SHUFFLE(2,3,0,1));
-    const __m128d sum2 = _mm_add_pd(sum1, shuffle2);
-    result = _mm_cvtsd_f64(sum2);
-    //    _mm_empty();
+    const __m128d shuffle = _mm_shuffle_pd(euclidean, euclidean, 1);
+    const __m128d sum = _mm_add_pd(euclidean, shuffle);
+    result = _mm_cvtsd_f64(sum);
     if (n)
         result += euclidean_remaining(x, y, n);	// remaining 0-1 entries
 
     return sqrt(result);
-
 }
--- main.cpp
+++ main.cpp
@@ -7,10 +7,6 @@ extern "C"
 #include "edcalc.h"
 }
 
-#include <glob.h>
-#include <immintrin.h>
-#include <math.h>
-
 using namespace std;
 
 struct Test
@@ -22,62 +18,28 @@ struct Test
         time = result = 0;
     }
 };
-double euclidean_remaining(const double *x,const double *y, size_t n);
 void fill_vector(double *x, double *y, size_t n);
-void fill_vector_control(double *x, double *y);
 void print_results(double time, double result);
 void _test(double(*method)(const double *,const double *, size_t ), const double *x,const double *y, size_t n);
 
 int main() {
 
-    size_t vector_size = 1000000;
+    size_t n = 1000000;
 
-    auto *x = (double*) aligned_alloc(32, vector_size * sizeof(double));
-    auto *y = (double*) aligned_alloc(32, vector_size * sizeof(double));
+    auto *x = (double*) aligned_alloc(32, n * sizeof(double));
+    auto *y = (double*) aligned_alloc(32, n * sizeof(double));
 
-//    fill_vector(vector_a, vector_b, vector_size);
-//
-//    cout << "BASELINE METHOD:\n" << endl;
-//    _test(&euclidean_naive, vector_a,vector_b,vector_size);
-//
-//    cout << "AVX2 WITH 256d :\n" << endl;
-//    _test(&euclidean_256d, vector_a,vector_b,vector_size);
-//
-//    cout << "SSE3 WITH 128d :\n" << endl;
-//    _test(&euclidean_128d, vector_a,vector_b,vector_size);
-//
-    cout << "CONTROL TEST:\n" << endl;
+    fill_vector(x, y, n);
 
-    fill_vector_control(x, y);
+    cout << "\n" << endl;
+    cout << "BASELINE METHOD:\n" << endl;
+    _test(&euclidean_naive, x,y,n);
 
-//    cout << "BASELINE CONTROL:\n" << endl;
-//    _test(&euclidean_naive, vector_a,vector_b,6);
-//
-//    cout << "AVX2 WITH 256d CONTROL:\n" << endl;
-//    _test(&euclidean_256d, vector_a,vector_b,6);
-    size_t n = 6;
+    cout << "AVX2 WITH 256d :\n" << endl;
+    _test(&euclidean_256d, x,y,n);
 
-    double result = 0;
-    __m256d euclidean = _mm256_setzero_pd();
-
-    for (; n>3; n-=4) {
-        const __m256d a = _mm256_load_pd(x);
-        const __m256d b = _mm256_load_pd(y);
-        const __m256d sub = _mm256_sub_pd(a,b);
-        const __m256d sqr = _mm256_mul_pd(sub, sub);
-        euclidean = _mm256_add_pd(euclidean, sqr);
-        x+=4;
-        y+=4;
-    }
-    const __m256d shuffle = _mm256_shuffle_pd(euclidean, euclidean, _MM_SHUFFLE(0,0,1,1)); // Latency: 1 Throughput: 1
-    const __m256d sum1 = _mm256_add_pd(shuffle, euclidean);                                // Latency: 4 Throughput: 0.5
-    const __m256d permute= _mm256_permute2f128_pd(sum1,sum1,1);                            // Latency: 3 Throughput: 1
-    const __m256d sum2 = _mm256_add_pd(sum1, permute);                                     // Latency: 4 Throughput: 0.5
-    result =  _mm256_cvtsd_f64(sum2);                                                      //         12             3
-    if (n)
-        result += euclidean_remaining(x, y, n);	// remaining 1-3 entries
-
-    result = sqrt(result);
+    cout << "SSE3 WITH 128d :\n" << endl;
+    _test(&euclidean_128d, x,y,n);
 
     return 0;
 }
@@ -86,7 +48,7 @@ void _test(double(*method)(const double *,const double *, size_t), const double 
 {
     Test test;
     StopWatch sw;
-    size_t qtd_tests = 1;
+    size_t qtd_tests = 10000;
 
     test.Reset();
     sw.Restart();
@@ -111,40 +73,8 @@ void fill_vector(double *x, double *y, size_t n)
     }
 }
 
-void fill_vector_control(double *x, double *y)
-{
-
-    //(67,3565−30,4699)²+(26,4998−27,4125)²+(82,8316−18,2401)²+(146,711−121,474)²+(135,973−138,431)²+(101,487−20,0173)²
-    //sqrt(12813,77610419)
-    //113,197950972
-
-    x[0] = 101.487;
-    y[0] = 20.0173;
-    x[1] = 135.973;
-    y[1] = 138.431;
-    x[2] = 146.711;
-    y[2] = 121.474;
-    x[3] = 82.8316;
-    y[3] = 18.2401;
-    x[4] = 26.4998;
-    y[4] = 27.4125;
-    x[5] = 67.3565;
-    y[5] = 30.4699;
-
-}
-
 void print_results(double time, double result){
     cout << "ELAPSED TIME:   " << time/1000    << "s" << endl;
     cout << "      RESULT:   " << result  << "\n" << endl;
     cout << "\n" << endl;
-}
-
-double euclidean_remaining(const double *x,const double *y, size_t n)
-{
-    double result = 0;
-    for(int i = 0; i < n; ++i){
-        const double num = x[i] - y[i];
-        result += num * num;
-    }
-    return result;
 }
