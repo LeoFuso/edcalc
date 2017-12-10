928137918f6ccafd95b18e72c81ffce12f440338
LeoFuso <leonardofusonuzzo@gmail.com>
2017-12-10 00:17:06 -0200

    yeah

--- edcalc.c
+++ edcalc.c
@@ -44,12 +44,12 @@ double euclidean_256d(const double *x,const double *y, size_t n)
         x+=4;
         y+=4;
     }
-    const __m256d shuffle1 = _mm256_shuffle_pd(euclidean, euclidean, _MM_SHUFFLE(1,0,3,2));
-    const __m256d sum1 = _mm256_add_pd(euclidean, shuffle1);
-    const __m256d shuffle2 = _mm256_shuffle_pd(sum1, sum1, _MM_SHUFFLE(2,3,0,1));
-    const __m256d sum2 = _mm256_add_pd(sum1, shuffle2);
-     result = _mm256_cvtsd_f64(sum2);
-    //    _mm_empty();
+    const __m256d shuffle1 = _mm256_shuffle_pd(euclidean, euclidean, _MM_SHUFFLE(0,0,1,1)); // Latency: 1 Throughput: 1
+    const __m256d sum1 = _mm256_add_pd(shuffle1, euclidean);                                // Latency: 4 Throughput: 0.5
+    const __m128d higherEnd= _mm256_extractf128_pd(sum1,1);                                 // Latency: 3 Throughput: 1
+    const __m256d castedHigherEnd =  _mm256_castpd128_pd256(higherEnd);                     // No cost
+    const __m256d sum2 = _mm256_add_pd(sum1, castedHigherEnd);                              // Latency: 4 Throughput: 0.5
+    result =  _mm256_cvtsd_f64(sum2);                                                       //         12             3
     if (n)
         result += euclidean_remaining(x, y, n);	// remaining 1-3 entries
 
