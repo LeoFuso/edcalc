34f7f20053811ecde19c85f5c8da226c83bf86f0
LeoFuso <leonardofusonuzzo@gmail.com>
2018-04-21 18:52:55 -0300

    Documentação realizada para o ed256.cpp

--- ednaive.cpp
+++ ednaive.cpp
@@ -4,11 +4,12 @@
 
 #include "ednaive.hpp"
 
-
-double Baseline::calculate(const double *x, const double *y, size_t n) {
-    asm("#inicio1");
+double
+Baseline::calculate(const double *x, const double *y, int n)
+{
     double result = 0;
-    for (; n > 0; --n) {
+    for (; n > 0; --n)
+    {
         const double num = x[n] - y[n];
         result += num * num;
     }
