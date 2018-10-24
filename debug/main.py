c579c22430ae37b9a8dc31cf9ffc2827dda66fe6
LeoFuso <leonardofusonuzzo@gmail.com>
2018-10-24 20:52:50 -0300

    issue-6 Eu troquei a chamada para o método correto

--- debug/main.py
+++ debug/main.py
@@ -1,8 +1,8 @@
 import DistancePy
 
-arr = [1, 2, 3, 4, 5, 6]
-arr2 = [1, 2, 3, 4, 5, 6]
+x = [1, 2, 3, 4, 5, 6]
+y = [1, 2, 3, 4, 5, 6]
 
 print("Calculate Sequence of:\nVALUE: ",arr)
-result = DistancePy.calculate(arr, arr2)
+result = DistancePy.euclidean(x, y)
 print("RESULT: ",result)
