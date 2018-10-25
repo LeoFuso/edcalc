a4c32597f2895e1e26714f1e84587ff041d3e686
LeoFuso <leonardofusonuzzo@gmail.com>
2018-10-24 21:12:26 -0300

    issue-6 Correção de variável não existente no código em python

--- debug/main.py
+++ debug/main.py
@@ -3,6 +3,6 @@ import DistancePy
 x = [1, 2, 3, 4, 5, 6]
 y = [1, 2, 3, 4, 5, 6]
 
-print("Calculate Sequence of:\nVALUE: ",arr)
+print("Calculate Sequence of:\nVALUE: ", x)
 result = DistancePy.euclidean(x, y)
-print("RESULT: ",result)
+print("RESULT: ", result)
