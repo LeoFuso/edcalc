c4d4fc14901d12ff6f7d7a48f0a0454aaa1b7304
LeoFuso <leonardofusonuzzo@gmail.com>
2018-10-25 00:16:51 -0300

    issue-6 Modifiquei o método de raiz quadrada (porque estava errado!)

--- distance-measure/distance_measure.hpp
+++ distance-measure/distance_measure.hpp
@@ -7,39 +7,38 @@ class DistanceMeasure
 {
 public:
 
-    DistanceMeasure() = default;
+	DistanceMeasure() = default;
 
-    virtual ~DistanceMeasure() = default;
+	virtual ~DistanceMeasure() = default;
 
-    virtual double
-    euclidean(const double *p, const double *q, unsigned long n) = 0;
+	virtual double
+	euclidean(const double *p, const double *q, unsigned long n) = 0;
 
-    /*
-     * Newton's method (also known as the Newton–Raphson method)
-     * to euclidean the square root of a positive number
-     */
-    double
-    sqrt(double t)
-    {
-        double r = t / 2;
-        int precision = 25;
+	/*
+	 * Newton's method (also known as the Newton–Raphson method)
+	 * to euclidean the square root of a positive number
+	 */
+	double
+	sqrt(double t)
+	{
+		double r = t / 2;
+		int precision = 25;
 
-        while (precision > 1)
-        {
-            r = (r + t / r) / 2;
-            precision -= 1;
-        }
-        return r;
-    }
+		for (; precision > 1; precision -= 1)
 
-    /*
+			r = (r + t / r) / 2;
+
+		return r;
+	}
+
+	/*
      * Returns the runtime name the object's class
      */
-    const char *
-    getClassName()
-    {
-        return typeid(*this).name();
-    }
+	const char *
+	getClassName()
+	{
+		return typeid(*this).name();
+	}
 };
 
 #endif //DISTANCE_MEASURE_HPP
