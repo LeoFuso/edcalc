5aff3fb076736bcd9cf61fb8616046a39a51d0de
LeoFuso <leonardofusonuzzo@gmail.com>
2018-10-26 11:32:18 -0300

    issue-11 Remoção dos artefatos produzidos atoa.

--- debug/main.cpp
+++ debug/main.cpp
@@ -1,27 +1,16 @@
 #include "../benchmark/benchmark.hpp"
-#include "../distance-measure/baseline_distance.hpp"
-#include "../distance-measure/__m128d__distance.hpp"
-#include "../distance-measure/__m256d__distance.hpp"
 #include "../adapter/cpp/DistanceMeasureFactory.hpp"
 
 int
 main()
 {
-	auto * baseline = new BaselineDistance;
-	auto * double128 = new __m128d__Distance;
-	auto * double256 = new __m256d__Distance;
-
-	unsigned long array_size = 1000000;
-	unsigned long num_tests = 10;
-
-	Benchmark benchmark(array_size, num_tests);
-
-	benchmark.perform(baseline);
-	benchmark.perform(double128);
-	benchmark.perform(double256);
-
 	auto *factory = new DistanceMeasureFactory();
 	auto *d_measure = factory->produce();
+
+	unsigned long array_size = 10000;
+	unsigned long num_tests = 100000;
+
+	Benchmark benchmark(array_size, num_tests);
 	benchmark.perform(d_measure);
 
 	return 0;
