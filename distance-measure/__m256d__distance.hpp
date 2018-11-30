46b3b51a40c705c2b19245d3654cbfc5198ccc8b
LeoFuso <leonardofusonuzzo@gmail.com>
2018-11-30 11:13:03 -0200

    issue-13 implementação avx2 dos métodos manhattan e cosine

--- distance-measure/__m256d__distance.hpp
+++ distance-measure/__m256d__distance.hpp
@@ -11,9 +11,13 @@ public:
 	euclidean(const double *, const double *, unsigned long) override;
 	double
 	manhattan(const double *p, const double *q, unsigned long n) override;
+	double
+	cosine(const double *p, const double *q, unsigned long n) override;
 private:
 	inline __m256d
 	_mm256_abs_pd(__m256d a);
+	inline double
+	_mm256_rdcsd_f64(__m256d a);
 };
 
 #endif //__m256D__DISTANCE_HPP
